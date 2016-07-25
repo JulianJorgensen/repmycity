5fa65c7cf8454003780525c771469efd6fa6349d
Julian Jorgensen <Julian@goldenchai.com>
2016-07-25 13:20:29 -0700

    Display products to homepage using http

--- _source/assets/javascript/controllers/home-controller.js
+++ _source/assets/javascript/controllers/home-controller.js
@@ -1,6 +1,10 @@
 // CONTROLLERS
-rmcApp.controller('homeController', ['$scope', function($scope) {
+rmcApp.controller('homeController', ['$scope', '$http', function($scope, $http) {
 
   console.log('home controller');
+  $http.get('/products.json').success(function (data) {
+    $scope.products = data.products;
+    console.log(data);
+  });
 
 }]);
